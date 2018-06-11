c5284fa537c9393297e8d47c7b36caa1da2ca2a0
GaetanRole <gaetan.role-dubruille@epitech.eu>
2018-06-11 16:17:05 +0200

    Adding FontAwesome last version, copy-webpack-plugin with one image test, updating components.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 var Encore = require('@symfony/webpack-encore');
 
+// Including CopyWebpackPlugin to manage static images
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
 Encore
     .setOutputPath('web/build/')
     .setPublicPath('/build')
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSassLoader()
+    .enableSourceMaps(!Encore.isProduction())
+    .autoProvidejQuery()
+    .enableVersioning()
     .createSharedEntry('vendor', ['jquery', 'bootstrap-sass', 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'])
     .addStyleEntry('style', './assets/scss/style.scss')
     .addEntry('main', './assets/js/main.js')
-    .autoProvidejQuery()
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSassLoader(function(sassOptions) {}, {
-        resolveUrlLoader: false
-    })
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning();
+    // Targeting images repository
+    .addPlugin(new CopyWebpackPlugin([
+        { from: './assets/images', to: 'images'}
+    ]));
+
 
 module.exports = Encore.getWebpackConfig();
